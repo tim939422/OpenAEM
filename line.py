1a83d843caae4962814c0a60f9894b2852db59e8
Duosi Fan <duosifan@hotmail.com>
2023-07-19 14:28:20 +0800

    complete line

--- line.py
+++ line.py
@@ -1,2 +1,77 @@
+import numpy as np
+
+
 class Line:
+    '''
+    A line parameterized by a 0 <= t <= 1
+
+        xv = (1-t)*p0 + t*p1
+        
+    '''
+    def __init__(self, p0, p1, ds=0.01):
+        self.p0 = p0.reshape(-1, 1)
+        self.p1 = p1.reshape(-1, 1)
+
+        
+        self.t = self.__discretize_tspace(ds)
+        self.np = np.size(self.t, axis=1) # num of points
+        self.nc = self.np - 1 # num of cells
+        self.ds = self.__compute_length()/len(self.t - 1)
+        
+    # public methods
+    def get_np(self):
+        return self.np
+    def get_nc(self):
+        return self.nc
+    def get_ds(self):
+        return self.ds
     
+    def get_length(self):
+        return self.__compute_length()
+    def get_xv(self):
+        return self.__compute_xv()
+    def get_xvd(self):
+        return self.__compute_xpvd()
+    
+    
+    # private methods
+    # this is overkill for just a simple straight line. However, they are
+    # provided here for further spline support
+    def __discretize_tspace(self, ds):
+        """discretize parametric space
+
+        Args:
+            ds (float): request spacing
+            
+        Returns:
+            float array: discretized parameter t
+        """
+        length = self.__compute_length()
+        n = int(np.ceil(length/ds))
+        t = np.linspace(0, 1, n + 1).reshape(1, -1)
+        return t
+        
+    def __compute_length(self):
+        """analytical length of a line
+
+        Returns:
+            float: line length
+        """        
+        return np.linalg.norm(self.p1 - self.p0)
+    
+    def __compute_xv(self):
+        """discretized point on the line
+
+        Returns:
+            float array: (3, M)
+        """        
+        return np.matmul(self.p0, 1 - self.t) + np.matmul(self.p1, self.t)
+    
+    def __compute_xpvd(self):
+        dot_xv = -self.p0 + self.p1
+        return np.repeat(dot_xv, self.np, axis=1)
+    
+if __name__ == '__main__':
+    p0 = np.array([0, 0, 0])
+    p1 = np.array([1, 0, 0])
+    line_1 = Line(p0, p1)
---
+++ test_line.py
@@ -0,0 +1,52 @@
+from pytest import approx
+import numpy as np
+
+from line import Line
+class Test_Line:
+    def test_get_length(self):
+        p0 = np.array([0, 0, 0])
+        p1 = np.array([1, 0, 0])
+        line_1 = Line(p0, p1)
+        assert(line_1.get_length() == approx(1.0))
+        
+        p0 = np.array([0, 0, 0])
+        p1 = np.array([1, 1, 1])
+        line_2 = Line(p0, p1)
+        assert(line_2.get_length() == approx(np.sqrt(3)))
+        
+    def test_dimension(self):
+        p0 = np.array([0, 0, 0])
+        p1 = np.array([1, 0, 0])
+        line = Line(p0, p1)
+        assert(line.get_nc() == 100)
+        assert(line.get_np() == 101)
+        
+    def test_get_xv(self):
+        p0 = np.array([0, 0, 0]).reshape(-1, 1)
+        p1 = np.array([1, 1, 1]).reshape(-1, 1)
+        line = Line(p0, p1)
+        xv = line.get_xv()
+        
+        # test if starting and ending points are on the line
+        assert(xv[:, 0].reshape(-1,1) == approx(p0))
+        assert(xv[:, -1].reshape(-1,1) == approx(p1))
+        
+        # test unit vector along the line
+        # problem with zero equality test: must be fixed in the future
+        t1 = (p1 - p0)/np.linalg.norm(p1 - p0)
+        pm = xv[:, np.random.choice(xv.shape[1])].reshape(-1, 1)
+        t2 = (pm - p0)/np.linalg.norm(pm - p0)
+        assert(approx(t1[0,0]) == approx(t2[0,0], 1e-6))
+        assert(approx(t1[1,0]) == approx(t2[1,0], 1e-6))
+        assert(approx(t1[2,0]) == approx(t2[2,0], 1e-6))
+        
+        
+    def test_get_xvd(self):
+        p0 = np.array([0, 0, 0]).reshape(-1, 1)
+        p1 = np.array([1, 1, 1]).reshape(-1, 1)
+        line = Line(p0, p1)
+        xvd = line.get_xvd()
+        assert(xvd[0, 0] == approx(1.0))
+        
+        
+        
